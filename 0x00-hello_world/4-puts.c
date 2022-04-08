376ec344deedd663317d42ba417f202c5253dd02
Lincoln Komen <komenlincoln@gmail.com>
2022-04-08 05:33:18 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ *
+ * Return: 0 always (exit main function)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
