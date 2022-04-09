6cf3c179f83cc42acdb2060407bf9643ba128ae7
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 08:20:56 -0700

    no printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, sizeof(str1));
+	write(2, str1, sizeof(str1[]));
 	return (1);
 }
