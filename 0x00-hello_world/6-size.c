be3762cee9a272c45df2726aac1cf87844af88c1
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 04:38:52 -0700

    hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void);
 {
-	printf("size of char: %d byte(s)\n", sizeof(char));
-	printf("size of int: %d byte(s)\n", sizeof(int));
-	printf("size of long int: %d byte(s)\n", sizeof(long int));
-	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
