17e186ef18aa2e9da13f7f4a78b4df3e78a257c3
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 08:17:24 -0700

    no printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-#include <sys/syscall.h>
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Return: Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char quest[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, 1, quest, sizeof(quest));
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, sizeof(str1));
 	return (1);
 }
