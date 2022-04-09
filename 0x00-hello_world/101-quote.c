81e0435c4b0cfa1fba230ec00c9aacf087d00d5c
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 07:48:08 -0700

    tryn sys to print statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <sys/syscall.h>
 /**
  *  main - Entry point
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, str, 59);
+	const char quest[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	syscall(SYS_write, 1, quest, sizeof(quest));
 	return (1);
 }
