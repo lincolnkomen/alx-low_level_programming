90c7953b8f507f8214f1bdf10268359fd6f876e3
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 14:20:37 -0700

    more typos

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	const char quest[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-9\n";
+	const char quest[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	syscall(SYS_write, 1, quest, sizeof(quest));
 	return 1;
 }
