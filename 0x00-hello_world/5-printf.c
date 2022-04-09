458f91c09d13be8fd34f7b564e9723950079c069
Lincoln Komen <komenlincoln@gmail.com>
2022-04-09 03:49:32 -0700

    positive

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * return : 0 to exit the main function
  */
-int main (void)
+int main(void)
 {
-	printf("\ "with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0)
 }
