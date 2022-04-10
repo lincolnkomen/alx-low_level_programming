b88be7a52c06491221e03dde2c697ff92c7bfa96
Lincoln Komen <komenlincoln@gmail.com>
2022-04-10 15:03:43 -0700

    zandeya, woo hoo!

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
 	}
-	else if (l == 0)
+	else if (last_digit == 0)
 	{
 		printf("Last digit of %i is %i and is 0\n", n, last_digit);
 	}
