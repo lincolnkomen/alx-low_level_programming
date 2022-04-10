726f0071671e22221d3f3f6c96c7689295dd09c2
Lincoln Komen <komenlincoln@gmail.com>
2022-04-10 13:29:21 -0700

    if & if..else task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* main - is the program entry point that defines wether a number is positive, negative or zero. */
+/* Return: Always 0 (Success) */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n==0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else (n<0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+## Variables, if, else & while
+This project aims in teaching arithmetic operators, relational operators, boolean operators, commenting, declaring & assigning variables and loops in C language.
