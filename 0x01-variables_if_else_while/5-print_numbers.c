cd27df0143fe9b89a8902f4cc8cccc403faf5c1c
Lincoln Komen <komenlincoln@gmail.com>
2022-04-11 10:10:23 -0700

    selena is me icone, mmh icone : )

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Program that prints single digits numbers of base 10 starting from 0
- * Return: 0
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
