38752ffe19aedf05640f3a238aa6b65c7169d7f8
Lincoln Komen <komenlincoln@gmail.com>
2022-04-11 09:11:01 -0700

    incrementing ints

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Program that prints single digits numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '10')
+	{
+		printf("%d i,\n i++");
+	}
+	return (0);
+}
