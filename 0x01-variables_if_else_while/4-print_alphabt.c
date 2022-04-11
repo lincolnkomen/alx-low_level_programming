422ab8207819e382aaa60cb6ca6553ff99727da4
Lincoln Komen <komenlincoln@gmail.com>
2022-04-11 08:53:13 -0700

    exclude and rule

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main bock
+ * Description: Print all letters except q and e
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char g = 'a';
+
+	while(g <= 'z')
+	{
+		if(g != 'q' && g != 'e')
+		{
+			putchar(g);
+		}
+			g++;
+	}
+	putchar('\n');
+	return (0);
+}
