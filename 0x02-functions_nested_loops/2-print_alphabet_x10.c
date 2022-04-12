9c56ee1eb39c13d8dd0ff78a390eb59aaacf3f73
Lincoln Komen <komenlincoln@gmail.com>
2022-04-12 11:57:49 -0700

    luca

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+{
+    int i, j = '0';
+
+    while(j < 10)
+    {
+        i = 'a';
+        while(i <= 'z')
+        {
+            putchar(i);
+            i++;
+        }
+        putchar("\n");
+        j++;
+    }
+}
