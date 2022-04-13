03621462d25b8ef384fff26800b11c63656fb29f
Lincoln Komen <komenlincoln@gmail.com>
2022-04-13 13:46:24 -0700

    wrong output

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
 	}
-	for (i = 97; i < 102; i++)
+	for (i = 97; i <= 102; i++)
 	{
 		putchar(i);
 	}
