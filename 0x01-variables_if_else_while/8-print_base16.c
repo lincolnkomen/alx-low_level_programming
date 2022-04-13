0de41db054875511083792f7c88d7332f07c4df7
Lincoln Komen <komenlincoln@gmail.com>
2022-04-13 13:42:24 -0700

    abcdef- gyle

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
