1fbc8b3fe33b3b8b5d410944b9eac295b28cf64d
Lincoln Komen <komenlincoln@gmail.com>
2022-04-13 13:26:43 -0700

    betty doc

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i = 'z';
 
-	while(i >= 'a')
+	while (i >= 'a')
 	{
 		putchar(i);
 		i--;
