c0b686306c569966fc69b41d5431a8a54c323560
Lincoln Komen <komenlincoln@gmail.com>
2022-04-20 10:31:59 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return: numbers
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	while (c = 0 && c <= 9)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
