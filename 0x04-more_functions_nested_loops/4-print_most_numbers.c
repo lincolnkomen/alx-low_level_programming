c52dea740fc6d7fbf7cb1c02d23ffb88cece65b2
Lincoln Komen <komenlincoln@gmail.com>
2022-04-21 03:05:16 -0700

    2 and 4 not in our plans

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ * Return: Nothing.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
