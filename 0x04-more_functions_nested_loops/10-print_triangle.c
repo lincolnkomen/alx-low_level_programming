fca98b1b0cfb1ff7dd39fa7d49e7416a61823d15
Lincoln Komen <komenlincoln@gmail.com>
2022-04-25 16:10:57 -0700

    print tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle.
+ * @size: number of lines.
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_pucthar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
