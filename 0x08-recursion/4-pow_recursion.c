d8b8710ff68119f07d81f0fe82eedac1b8f7669a
Lincoln Komen <komenlincoln@gmail.com>
2022-05-16 07:20:34 -0700

    thanos

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns power
+ * @x: base
+ * @y: exponent
+ * Return: the answer
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
